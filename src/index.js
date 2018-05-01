e932e928510c7548a38d5cbfa057caefa4f4ab55
Peng Lyu <penn.lv@gmail.com>
2018-05-01 11:21:19 -0700

    0.2.0 use syntaxColor

--- src/index.js
+++ src/index.js
@@ -1,141 +1,275 @@
 const nova = require('nova-colors')
 
-const sourceString =`<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>name</key>
-	<string>nova.ansiGroups</string>
-	<key>settings</key>
-	<array>
-		<dict>
-			<key>settings</key>
-			<dict>
-				<key>background</key>
-				<string>${nova.ansiGroups.normal.black}</string>
-				<key>foreground</key>
-				<string>${nova.ansiGroups.bright.black}</string>
-				<key>caret</key>
-				<string>${nova.ansiGroups.normal.red}</string>
-				<key>invisibles</key>
-				<string>${nova.default.grays.gray2}</string>
-				<key>lineHighlight</key>
-				<string>${nova.default.grays.gray2}</string>
-				<key>selection</key>
-				<string>${nova.ansiGroups.normal.cyan}7F</string>
-				<key>findMatchHighlight</key>
-				<string>${nova.ansiGroups.normal.cyan}40</string>
-				<key>rangeHighlight</key>
-				<string>${nova.ansiGroups.normal.cyan}40</string>
-				<key>currentFindMatchHighlight</key>
-				<string>${nova.ansiGroups.normal.cyan}7F</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>TOKEN</string>
-			<key>scope</key>
-			<string>token</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.ansiGroups.normal.white}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>CONSTANT</string>
-			<key>scope</key>
-			<string>string.single, string.template, string.double, string.regex, constant, numeric, boolean, punctuation.string, variable.property, variable.other, dictionary.value, css.property-value, markup.link, markup.raw, html.id, meta.function.type.parameter, meta.indexer.parameter</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.syntaxGroups.constant}</string>
-			</dict>
-		</dict>	
-		<dict>
-			<key>name</key>
-			<string>IDENTIFIER</string>
-			<key>scope</key>
-			<string>variable, function.entity, object.key, variable, entity.class, entity.name.tag, css.attribute-name, css.tag, jsx.attribute-name, html.tag</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.syntaxGroups.identifier}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>STATEMENT</string>
-			<key>scope</key>
-			<string>keyword, control, operator, punctuation.operator, css.property-name, jsx.component, jsx.tag, html.attribute-name, entity.other.attribute-name</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.syntaxGroups.statement}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>TYPE</string>
-			<key>scope</key>
-			<string>storage, undefined, null, nan, entity.name.class, meta.enum</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.syntaxGroups.type}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>GLOBAL</string>
-			<key>scope</key>
-			<string> module, import, include, dom, this, builtin, process </string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.ansiGroups.normal.magenta}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>EMPHASIS</string>
-			<key>scope</key>
-			<string>css.pseudo-class, markup.italic, markup.bold</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.ansiGroups.bright.magenta}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>SPECIAL</string>
-			<key>scope</key>
-			<string>arrow, punctuation.parameters, quasi.begin, quasi.end, dictionary.begin, dictionary.end, module.all, css.property-list, css.function, markup.heading, list</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.syntaxGroups.special}</string>
-			</dict>
-		</dict>
-		<dict>
-			<key>name</key>
-			<string>TRIVIAL</string>
-			<key>scope</key>
-			<string>comment, punctuation</string>
-			<key>settings</key>
-			<dict>
-				<key>foreground</key>
-				<string>${nova.syntaxGroups.trivial}</string>
-			</dict>
-		</dict>
-		</array>
-	<key>uuid</key>
-	<string>f0585502-0d66-4ff4-b3f4-ae6ffb40832e</string>
-</dict>
-</plist>
-
-`
+const sourceString =`{
+	"$schema": "vscode://schemas/color-theme",
+	"name": "Nova",
+	"colors": {
+		"activityBarBadge.background": "#7FC1CA",
+		"activityBarBadge.foreground": "#3C4C55",
+		"badge.background": "#7FC1CA",
+		"badge.foreground": "#3C4C55",
+		"button.background": "#7FC1CA",
+		"button.foreground": "#3C4C55",
+		"debugToolBar.background": "#1E272C",
+		"debugExceptionWidget.background": "#DF8C8C",
+		"debugExceptionWidget.border": "#DF8C8C",
+		"dropdown.foreground": "#7FC1CA",
+		"editor.background": "${nova.uiGroups.background}",
+		"editor.foreground": "${nova.uiGroups.foreground}",
+		"editorLineNumber.activeForeground": "#7FC1CA",
+		"editor.lineHighlightBackground": "#556873",
+		"editor.selectionBackground": "#6A7D89",
+		"editor.findMatchBackground": "#6A7D89",
+		"editor.wordHighlightBackground": "#556873",
+		"editor.findRangeHighlightBackground": "#556873",
+		"editor.inactiveSelectionBackground": "#556873",
+		"editor.wordHighlightStrongBackground": "#556874",
+		"editor.findMatchHighlightBackground": "#6A7D89",
+		"editorLineNumber.foreground": "#6A7D89",
+		"editorGroup.background": "#1E272C",
+		"editorGroupHeader.noTabsBackground": "#1E272C",
+		"editorGroupHeader.tabsBackground": "#1E272C",
+		"editorWidget.border": "${nova.uiGroups.background}",
+		"editorWidget.background": "${nova.uiGroups.background}",
+		"editorGroup.border": "#899BA6",
+		"extensionButton.prominentBackground": "#7FC1CA",
+		"extensionButton.prominentForeground": "#3C4C55",
+		"extensionButton.prominentHoverBackground": "#7FC1CA",
+		"sideBar.background": "#1E272C",
+		"activityBar.background": "#1E272C",
+		"tab.activeBackground": "${nova.uiGroups.background}",
+		"tab.activeForeground": "#7FC1CA",
+		"tab.inactiveBackground": "#1E272C",
+		"tab.inactiveForeground": "#6A7D89",
+		"tab.border": "#1E272C",
+		"tab.activeBorder": "${nova.uiGroups.background}",
+		"panelTitle.activeForeground": "#7FC1CA",
+		"scrollbarSlider.background": "#7FC1CA",
+		"list.activeSelectionBackground": "#7FC1CA",
+		"list.activeSelectionForeground": "#3C4C55",
+		"list.inactiveSelectionBackground": "#3C4C55",
+		"list.inactiveSelectionForeground": "#7FC1CA",
+		"list.hoverBackground": "#6A7D89",
+		"list.focusBackground": "#1E272C",
+		"list.focusForeground": "#7FC1CA",
+		"list.highlightForeground": "#7FC1CA",
+		"progressBar.background": "#7FC1CA",
+		"input.background": "#556873",
+		"input.border": "#556873",
+		"input.foreground": "#7FC1CA",
+		"input.placeholderForeground": "#d8dee9",
+		"inputValidation.errorBackground": "#DF8C8C",
+		"inputValidation.errorBorder": "#DF8C8C",
+		"editorIndentGuide.background": "#556873",
+		"inputOption.activeBorder": "#7FC1CA",
+		"notification.errorBackground": "#DF8C8C",
+		"notification.errorForeground": "#3C4C55",
+		"notification.warningBackground": "#F2C38F",
+		"notification.warningForeground": "#3C4C55",
+		"notification.background": "#3C4C55",
+		"notification.buttonBackground": "#556873",
+		"peekViewTitle.background": "#1E272C",
+		"peekView.border": "#1E272C",
+		"peekViewEditor.background": "#3C4C55",
+		"peekViewResult.background": "#1E272C",
+		"peekViewResult.selectionBackground": "#7FC1CA",
+		"peekViewResult.selectionForeground": "#3C4C55",
+		"panelTitle.activeBorder": "#7FC1CA",
+		"statusBar.background": "#1E272C",
+		"statusBar.debuggingBackground": "#DF8C8C",
+		"terminal.background": "#3C4C55",
+		"terminal.foreground": "#C5D4DD",
+		"terminal.ansiBlack": "#3C4C55",
+		"terminal.ansiRed": "#DF8C8C",
+		"terminal.ansiGreen": "#A8CE93",
+		"terminal.ansiYellow": "#DADA93",
+		"terminal.ansiBlue": "#83AFE5",
+		"terminal.ansiMagenta": "#9A93E1",
+		"terminal.ansiCyan": "#7FC1CA",
+		"terminal.ansiWhite": "#C5D4DD",
+		"terminal.ansiBrightBlack": "#899BA6",
+		"terminal.ansiBrightMagenta": "#D18EC2",
+		"terminal.ansiBrightWhite": "#E6EEF3",
+		"titleBar.activeBackground": "#1E272C",
+		"widget.shadow": "#00000066",
+		"diffEditor.insertedTextBackground": "#A8CE930C",
+		"diffEditor.insertedTextBorder": "#A8CE93",
+		"diffEditor.removedTextBackground": "#DF8C8C0C",
+		"diffEditor.removedTextBorder": "#DF8C8C",
+		"editorHoverWidget.background": "#556873",
+		"editorHoverWidget.border": "#3C4C55",
+		"editorSuggestWidget.highlightForeground": "#7FC1CA",
+		"editorSuggestWidget.background": "#556873",
+		"editorSuggestWidget.border": "#556873",
+		"editorSuggestWidget.selectedBackground": "#6A7D89",
+		"editorOverviewRuler.addedForeground": "#A8CE93",
+		"editorOverviewRuler.deletedForeground": "#DF8C8C",
+		"editorOverviewRuler.modifiedForeground": "#F2C38F",
+		"editorGutter.addedBackground": "#A8CE93",
+		"editorGutter.deletedBackground": "#DF8C8C",
+		"editorGutter.modifiedBackground": "#83AFE5"
+	},
+	"tokenColors": [
+		{
+			"scope": "emphasis",
+			"settings": {
+				"fontStyle": "italic",
+				"foreground": "${nova.syntaxGroups.emphasis}"
+			}
+		},
+		{
+			"scope": "strong",
+			"settings": {
+				"fontStyle": "bold"
+			}
+		},
+		{
+			"scope": [
+				"comment",
+				"meta.selector"
+			],
+			"settings": {
+				"foreground": "${nova.default.grays.gray4}"
+			}
+		},
+		{
+			"scope": [
+				"constant.language",
+				"constant.numeric",
+				"string",
+				"markup.inline.raw",
+				"meta.brace.square"
+			],
+			"settings": {
+				"foreground": "${nova.syntaxGroups.constant}"
+			}
+		},
+		{
+			"scope": "entity.name.tag.css",
+			"settings": {
+				"foreground": "${nova.syntaxGroups.special}"
+			}
+		},
+		{
+			"scope": [
+				"entity.other.attribute-name.class.css",
+				"entity.other.attribute-name.class.mixin.css",
+				"entity.other.attribute-name.id.css",
+				"entity.other.attribute-name.parent-selector.css",
+				"entity.other.attribute-name.pseudo-class.css",
+				"entity.other.attribute-name.pseudo-element.css",
+				"source.css.less entity.other.attribute-name.id",
+				"entity.other.attribute-name.attribute.scss",
+				"entity.other.attribute-name.scss"
+			],
+			"settings": {
+				"foreground": "${nova.syntaxGroups.emphasis}"
+			}
+		},
+		{
+			"scope": "markup",
+			"settings": {
+				"foreground": "${nova.syntaxGroups.emphasis}"
+			}
+		},
+		{
+			"scope": "markup.underline",
+			"settings": {
+				"fontStyle": "underline",
+			}
+		},
+		{
+			"scope": "markup.bold",
+			"settings": {
+				"fontStyle": "bold",
+			}
+		},
+		{
+			"scope": "markup.italic",
+			"settings": {
+				"fontStyle": "italic",
+			}
+		},
+		{
+			"scope": "markup.heading",
+			"settings": {
+				"fontStyle": "bold",
+				"foreground": "${nova.syntaxGroups.special}"
+			}
+		},
+		{
+			"scope": "storage",
+			"settings": {
+				"foreground": "${nova.syntaxGroups.type}"
+			}
+		},
+		{
+			"name": "String interpolation",
+			"scope": [
+				"header",
+				"markup.heading.markdown",
+				"beginning.punctuation.definition.quote.markdown",
+				"beginning.punctuation.definition.list.markdown",
+				"punctuation.definition.tag",
+				"punctuation.definition.template-expression.begin",
+				"punctuation.definition.template-expression.end",
+				"punctuation.section.embedded",
+				"punctuation.definition.block",
+				"entity.name.tag.tsx",
+				"meta.brace.round",
+				"storage.type.function.arrow"
+			],
+			"settings": {
+				"foreground": "${nova.syntaxGroups.special}"
+			}
+		},
+		{
+			"scope": [
+				"support.function",
+				"entity.name.function",
+				"keyword.operator.assignment"
+			],
+			"settings": {
+				"foreground": "${nova.syntaxGroups.statement}"
+			}
+		},
+		{
+			"scope": [
+				"variable.language",
+				"keyword",
+				"support.class",
+				"entity.name.class",
+				"entity.name.type.class"
+			],
+			"settings": {
+				"foreground": "${nova.syntaxGroups.global}" 
+			}
+		},
+		{
+			"scope": [
+				"entity.other.attribute-name.tsx",
+				"support.type.property-name",
+				"meta.object-literal.key",
+				"entity.name.function",
+				"entity.name.tag",
+				"meta.definition.variable",
+				"meta.function.expression",
+				"variable.parameter",
+				"variable.other.readwrite source",
+				"variable.other.readwrite.alias",
+				"variable.other.object"
+			],
+			"settings": {
+				"foreground": "${nova.syntaxGroups.identifier}"
+			}
+		},
+		{
+			"scope": "support.variable.property",
+			"settings": {
+				"foreground": "${nova.default.grays.gray5}"
+			}
+		}
+	]
+}`
 
 process.stdout.write(sourceString)
